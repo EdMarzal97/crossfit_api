4fc5366e9a1f135ea48010dc243de9d7e71e3038
Edward Marzal <edsanchezmarzal@gmail.com>
2022-07-30 14:17:38 -0500

    routes for versions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 
+const v1Router = require("./version-1/routes");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // this is for testing purposes only
-app.get("/", (req, res) => {
-    res.send("<h1>OHAYO SEKAI GOOD MORNING WORLD!</h1>");
-});
+//app.get("/", (req, res) => {
+//    res.send("<h1>GOOD MORNING WORLD!</h1>");
+//});
+
+app.use("/api/v1", v1Router);
 
 app.listen(PORT, () => {
     console.log(`API is listening on port ${PORT}`);
